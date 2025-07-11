1315ab7e0a098582544b2761b5a74c4cddc0fbae
Lillypad17 <stefanievlna17@gmail.com>
2025-07-11 23:30:40 +0000

    modified lab 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -52,6 +52,35 @@ class Program
         }
 
         //Part 4
-        
+        Console.WriteLine("Enter a number (1-7) for the day of the week:");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid Day!");
+                break;
+        }
     }
 }
